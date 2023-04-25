e0df4d1455a8ebfdd82da3af1af95bb7174a59fd
JulienHanon <hanonjulien@outlook.fr>
2023-04-25 11:50:38 +0200

    fix: 🐛 Résolution problème tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+
+ 
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  
   plugins: [],
 }
-
