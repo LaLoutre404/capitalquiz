59caf52ef19c8dbbfca03eed4034273a93e5eccd
JulienHanon <hanonjulien@outlook.fr>
2023-04-25 09:14:00 +0200

    feat: 🎸 Creation du component ProgressBarComponents
    Ajout d'un component qui contient une bar de progression en fonction de
    la question et d'un bouton valider

---
+++ src/components/ProgressBarComponents.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export const ProgressBarComponent = (props:
+    { nbQuestion: number}
+  ) => {
+
+    const [progress, setProgress] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(1)
+
+    const move = (start: number, end: number, total: number) => {
+        const goal = 100 * end / total;
+        const id = setInterval(() => {
+        setProgress(prevProgress => {
+            if (prevProgress >= goal) {
+            clearInterval(id);
+            return prevProgress;
+            } else {
+            return prevProgress + 1;
+            }
+        });
+        setCurrentQuestion(currentQuestion + 1);
+        }, 10); //delay = vitesse de progression de la bar
+    };
+
+   
+    return(
+        <>
+            <button onClick={() => move(progress, progress + 10, props.nbQuestion * 10)}>Valider</button>
+            <div>
+                Question {currentQuestion} / {props.nbQuestion}
+            </div>
+            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                <div style={{ width: `${progress}%` }} className="bg-blue-600 h-2.5 rounded-full"></div>
+            </div>
+        </> 
+    )
+}
